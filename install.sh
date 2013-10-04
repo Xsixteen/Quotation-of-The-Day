29a081803e0e4bb80827e9fd3f2d11249cc8b515
Xsixteen <x16.tux@gmail.com>
2013-10-04 00:28:15 +0000

    Updates to the Install Script

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 echo -n "Please enter install path: "
 read path
+ABS_PATH=$(cd $path; pwd)
+echo -e "\nPath Identified as $ABS_PATH"
 
 echo -n "Please enter mysql username: "
 read sql_user
@@ -20,3 +22,14 @@ echo -e "\t=Installed log to $path/log\n"
 echo -e "\n===SQL Installation===\n"
 echo -e "\t=Attempting to populate DB\n"
 mysql -u$sql_user -p$sql_pass < ./sql/quotation_db.sql
+
+echo -e "\n===Creating Cron Jobs\n"
+mkdir tmp
+echo -e "\t=Creating Bash Script\n"
+echo "#!/bin/bash" >> ./tmp/quotation_of_the_day.sh
+echo "php $ABS_PATH/bin/updateQuote.php >> $ABS_PATH/log/quotation_log"  >> ./tmp/quotation_of_the_day.sh
+echo -e "\t=Installing Bash Script\n"
+chmod +x ./tmp/quotation_of_the_day.sh
+mv ./tmp/quotation_of_the_day.sh /etc/cron.hourly
+rm -fr ./tmp
+echo -e "\t=Installation Complete"
